92bf5ad68721392acdf7d282bb15a45f79ff147a
rob22202 <rob22202@gmail.com>
2013-05-24 11:53:10 -0300

    Update Autogator.py

--- Autogator.py
+++ Autogator.py
@@ -2,7 +2,7 @@
 
 # 99% of this code derived from Automator by 1aN0rmus from http://www.tekdefense.com/
 # This verion is made for mass ip information lookups and outputs in tabular format 
-# The original (with lots more bells and whitstles than Autogator) at https://github.com/1aN0rmus/TekDefense/blob/master/Automater.py
+# The original (with many more bells and whitstles than Autogator) is at https://github.com/1aN0rmus/TekDefense/blob/master/Automater.py
 # Usage: Autogator.py -i <inputfile>
 # The input file should contain a \n delimited list of ip addresses.
 # rob22202@gmail.com, rob22202 on github
